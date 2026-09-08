656deae839a55935648f49512991d9d7a592abe5
Nvx12 <raslensafsaf@gmail.com>
2026-09-08 17:28:38 +0200

    chore: migrate state backend to Terraform Cloud and update CI/CD pipeline for automated plans and applies

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,21 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-state"
-    key    = "prod/terraform.tfstate"
+  cloud {
+    organization = "terraform-workshop-1"
 
-    region = "us-east-1"
-
-    endpoints = {
-      s3 = "http://localhost:9000"
+    workspaces {
+      name = "terraform-prod"
     }
-
-
-    use_lockfile = true
-
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    use_path_style              = true
   }
 }
